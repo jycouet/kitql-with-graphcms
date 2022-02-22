eb1593d16f399e75ec9b900e0807e1b25452af9e
jycouet <jycouet@gmail.com>
2022-02-22 21:26:56 +0100

    adding queries (client side)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,31 +1,29 @@
 <script lang="ts" context="module">
+  import { browser } from '$app/env'
   import {
     AllPostsQuery,
     AllPostsQueryStore,
   } from '$lib/graphql/_kitql/graphqlStores'
-
-  // export const load = async ({ fetch }) => {
-  //   await AllPostsQuery({ fetch })
-  //   return {}
-  // }
 </script>
 
 <script lang="ts">
-  // let posts = $AllPostsQueryStore.data?.posts
+  $: browser && AllPostsQuery({})
+  $: posts = $AllPostsQueryStore?.data?.posts
 </script>
 
 <h1>Welcome to KitQL</h1>
 
-<!-- <ul>
-  {#each posts as { title, excerpt, slug }}
+<ul>
+  {#each posts ?? [] as { title, excerpt, slug }}
     <li>
-      <a class="nice" sveltekit:prefetch href={`/posts/${slug}`}>
+      <a class="nice" href={`/posts/${slug}`}>
         <h3>{title}</h3>
         <div class="subtitle">{excerpt}</div>
       </a>
     </li>
   {/each}
-</ul> -->
+</ul>
+
 <style>
   li {
     margin-bottom: 1rem;
